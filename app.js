bb2f543759f3c644d519cb66f740fe83f91b13c7
anurag071111 <anuragpandeyuna@gmail.com>
2025-02-23 01:24:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
+app.set("view engine", "ejs");
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+app.get("/best", (req, res) => {
+  res.render("best");
+});
