6de5c69e080807a7dcc0423a5f5f20226400e0d6
anurag071111 <anuragpandeyuna@gmail.com>
2025-02-23 01:37:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
 });
 app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("home");
+});
 app.get("/home", (req, res) => {
   res.render("home");
 });
